81559a1e184bf87c9cb2cc56f367f91763284cf0
Alejandro Perez Pujante <perezpujantealejandro1@gmail.com>
2024-08-23 17:18:06 +0200

    add base site/seo data

---
+++ src/modules/seo/site-data.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Tic-Tac-Launch'
+export const SITE_DESCRIPTION = 'Tic-Tac-Toe built with Astro and fully accesible!'
+export const SITE_AUTHOR = 'Alejandro Pérez Pujante'
+export const SITE_KEYWORDS = 'astro, tic-tac-toe, game, accesible, vue'
