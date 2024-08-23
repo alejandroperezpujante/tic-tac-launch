c90327ca267b2d99bc9ec3f978192e64d0269edc
Alejandro Perez Pujante <perezpujantealejandro1@gmail.com>
2024-08-23 17:18:43 +0200

    core site layout

---
+++ src/modules/core/index.ts
@@ -0,0 +1,13 @@
+import Header from './SiteCoreHeader.astro';
+import Content from './SiteCoreContent.astro';
+import Footer from './SiteCoreFooter.astro';
+
+export {
+	Header,
+	Content,
+	Footer,
+
+	Header as SiteHeader,
+	Content as SiteContent,
+	Footer as SiteFooter
+}
