aa841da5333767659cb481ea95b8e78d42cf7569
Alejandro Perez Pujante <perezpujantealejandro1@gmail.com>
2024-08-23 17:17:00 +0200

    add custom tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,41 @@
 import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 const config: Config = {
 	content: ['./src/**/*.{astro,html,js,ts,vue}'],
 	theme: {
-		extend: {},
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
+		extend: {
+			screens: {
+				'high-dpi': {
+					raw: 'only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min-resolution: 2dppx)'
+				}
+			},
+			colors: {
+				background: 'oklch(var(--background) / <alpha-value>)',
+				foreground: 'oklch(var(--foreground) / <alpha-value>)',
+				border: 'oklch(var(--border) / <alpha-value>)',
+				card: {
+					DEFAULT: 'oklch(var(--card) / <alpha-value>)',
+					foreground: 'oklch(var(--card-foreground) / <alpha-value>)'
+				},
+				muted: {
+					DEFAULT: 'oklch(var(--muted) / <alpha-value>)',
+					foreground: 'oklch(var(--muted-foreground) / <alpha-value>)'
+				}
+			},
+			fontFamily: {
+				headline: ['Tiny5', ...fontFamily.sans]
+			}
+		}
 	},
-	plugins: [],
+	plugins: []
 }
 
 export default config
